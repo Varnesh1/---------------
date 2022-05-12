71f7774248ccfdcb5c29d6fb88330a8b5ee1e197
Varnesh1 <100467880+Varnesh1@users.noreply.github.com>
2022-05-12 19:44:39 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload() {
+  
+}
+
+function setup() {
+createCanvas(600, 200);
+
+//create a trex sprite
+
+  
+}
+
+function draw() {
+background(220);
+Text("")
+//jump when the space button is pressed
+
+}
